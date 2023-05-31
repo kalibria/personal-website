711b205af99a97602b378c797c49a1779171e593
Maria Kazakova <mariakazakova92@gmail.com>
2023-05-31 16:30:23 +0300

    started to mark up the Resume page. Added main and sidebar

---
+++ components/MainInfo.tsx
@@ -0,0 +1,11 @@
+export default function MainInfo(){
+  return <>
+  <main>
+    <header>
+      <h1>Maria Kushlianskaya</h1>
+      <h2>Junior frontend developer</h2>
+    </header>
+
+  </main>
+  </>
+}
---
+++ pages/resume.tsx
@@ -0,0 +1,8 @@
+import MainInfo from 'components/MainInfo';
+
+export default function Resume() {
+  return (<>
+    <MainInfo/>
+    <aside></aside>
+  </>)
+}
