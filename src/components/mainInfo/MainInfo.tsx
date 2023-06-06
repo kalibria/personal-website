2707426cfa3f0396ca8e65deeeffd3d6bf15b1df
Maria Kazakova <mariakazakova92@gmail.com>
2023-06-06 16:44:47 +0300

    murked up mainInfo page

--- src/components/mainInfo/MainInfo.tsx
+++ src/components/mainInfo/MainInfo.tsx
@@ -7,7 +7,32 @@ export default function MainInfo(){
       <h1 className={styles.textHeaders}>Maria Kushlianskaya</h1>
       <p className={styles.text}>Junior frontend developer</p>
     </header>
-
+    <article>
+      <h3>
+        PROFESSIONAL EXPERIENCE
+      </h3>
+      <hr/>
+      <p>Lorem ipsum</p>
+    </article>
+    <article>
+      <h3>
+        WORK EXPERIENCE
+      </h3>
+      <hr/>
+      <h4>Lorem Ipsum</h4>
+      <p>Lorem ipsum</p>
+      <h4>Lorem Ipsum</h4>
+      <p>Lorem ipsum</p>
+      <h4>Lorem Ipsum</h4>
+      <p>Lorem ipsum</p>
+      <h4>Lorem Ipsum</h4>
+      <p>Lorem ipsum</p>
+    </article>
+    <article>
+      <h3>EDUCATION</h3>
+      <hr/>
+      <p4>Lorem ipsum</p4>
+    </article>
   </main>
   </>
 }
