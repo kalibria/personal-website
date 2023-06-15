ca0ecf86afb31f3433f56cace5fc0e33f2e06d27
Maria Kazakova <mariakazakova92@gmail.com>
2023-06-15 14:16:12 +0300

    added info about professional experience

--- src/components/asideBar/AsideBar.tsx
+++ src/components/asideBar/AsideBar.tsx
@@ -5,37 +5,55 @@ import {faEnvelope} from '@fortawesome/free-solid-svg-icons';
 import {faGlobe} from '@fortawesome/free-solid-svg-icons';
 import {faHouse} from '@fortawesome/free-solid-svg-icons';
 
+
 export default function AsideBar () {
   return (<aside className={styles.asideBarContainer}>
     <div className={styles.myPhotoContainer}/>
 
     <ul className={styles.contactsList}>
       <li className={styles.contactListItem}><FontAwesomeIcon icon={faPhone} className={styles.listIcon} />phone number</li>
-      <li className={styles.contactListItem}><FontAwesomeIcon icon={faEnvelope} className={styles.listIcon}/>email</li>
+      <li className={styles.contactListItem}><FontAwesomeIcon icon={faEnvelope} className={styles.listIcon}/>mariakazakova92@gmail.com</li>
       <li className={styles.contactListItem}><FontAwesomeIcon icon={faGlobe} className={styles.listIcon}/>website</li>
       <li className={styles.contactListItem}><FontAwesomeIcon icon={faHouse} className={styles.listIcon}/>address</li>
     </ul>
 
     <div>
-      <h3>SKILLS</h3>
+      <h3 className={styles.subtitle}>SKILLS</h3>
       <hr/>
+      <h4 className={styles.subtitle2}>Languages</h4>
       <ul className={styles.list}>
-        <li className={styles.listItem}>Bookkeeping</li>
-        <li className={styles.listItem}>Tax Filling</li>
-        <li className={styles.listItem}>Data Entry</li>
-        <li className={styles.listItem}>Data Analysis</li>
-        <li className={styles.listItem}>Public Accounting</li>
-        <li className={styles.listItem}>Business Assessment</li>
+        <li className={styles.listItem}>HTML</li>
+        <li className={styles.listItem}>CSS</li>
+        <li className={styles.listItem}>Javascript</li>
+        <li className={styles.listItem}>TypeScript</li>
+
+      </ul>
+
+      <h4 className={styles.subtitle2}>Libraries</h4>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>React</li>
+        <li className={styles.listItem}>Redux</li>
+      </ul>
+
+      <h4 className={styles.subtitle2}>Testing</h4>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>Jest</li>
+        <li className={styles.listItem}>React testing library</li>
+      </ul>
+
+      <h4 className={styles.subtitle2}>CI/CD</h4>
+      <ul className={styles.list}>
+        <li className={styles.listItem}>GIT</li>
       </ul>
     </div>
 
     <div>
-      <h3>LANGUAGES</h3>
+      <h3 className={styles.subtitle}>LANGUAGES</h3>
       <hr/>
       <ul className={styles.list}>
-        <li className={styles.listItem}>English</li>
-        <li className={styles.listItem}>Russian</li>
-        <li className={styles.listItem}>Poland</li>
+        <li className={styles.listItem}>Russian - native</li>
+        <li className={styles.listItem}>English - intermediate</li>
+        <li className={styles.listItem}>Poland - elementary</li>
       </ul>
     </div>
 
--- src/components/mainInfo/MainInfo.tsx
+++ src/components/mainInfo/MainInfo.tsx
@@ -5,31 +5,38 @@ export default function MainInfo(){
   <main className={styles.mainContainer}>
     <header>
       <h1 className={styles.textHeaders}>MARIA KUSHLIANSKAYA</h1>
-      <p className={styles.text}>Junior frontend developer</p>
+      <p className={styles.text}>FRONT-END DEVELOPER</p>
     </header>
     <article>
-      <h3>
+      <h3 className={styles.subtitle}>
         PROFESSIONAL EXPERIENCE
       </h3>
       <hr/>
-      <p>Lorem ipsum</p>
+      <p>Front-End Developer has portfolio with pet projects <a href={'https://github.com/kalibria'}>( https://github.com/kalibria)</a>. Started with a layout of the simplest pages using HTML and CSS.</p>
+      <p>Step by step dived into front-end development and in their subsequent projects used <b>JavaScript, TypeScript, React, Redux, Redux Toolkit, CSS Grid,Css Flex, SASS, API requests.</b> Met <b>regular expressions, environment variables, local storage, BEM methodology, adaptive layout, Webpack, ES Modules, Bootstrap, Tailwind, Formik </b>and its. </p>
+      <p>One of my latest projects is <b>Honda Tracker - booking app</b> <a href={'https://github.com/ilya-labs/Honda_tracker'}>(https://github.com/ilya-labs/Honda_tracker)</a>. Working on it  I used  <b>Jira, Git</b>, opened PRs, my mentor reviewed code.</p>
+      <p>This app write in  React, Typescript, CSS: <b>Tailwind</b>, Forms: <b>Formik, Intl.DateTimeFormat, RTK query, Postman client</b> for backend requests.</p>
+
+       <p>Link to my GitHub <a href={'https://github.com/kalibria'}> https://github.com/kalibria.</a></p>
+
+      <p>For now I am working on my personal web-site. It will be adaptive and I will use TypeScript, JavaScript, React, Redux, <b>Next.js.</b></p>
     </article>
+    {/*<article>*/}
+    {/*  <h3 className={styles.subtitle}>*/}
+    {/*    WORK EXPERIENCE*/}
+    {/*  </h3>*/}
+    {/*  <hr/>*/}
+    {/*  <h4>Lorem Ipsum</h4>*/}
+    {/*  <p>Lorem ipsum</p>*/}
+    {/*  <h4>Lorem Ipsum</h4>*/}
+    {/*  <p>Lorem ipsum</p>*/}
+    {/*  <h4>Lorem Ipsum</h4>*/}
+    {/*  <p>Lorem ipsum</p>*/}
+    {/*  <h4>Lorem Ipsum</h4>*/}
+    {/*  <p>Lorem ipsum</p>*/}
+    {/*</article>*/}
     <article>
-      <h3>
-        WORK EXPERIENCE
-      </h3>
-      <hr/>
-      <h4>Lorem Ipsum</h4>
-      <p>Lorem ipsum</p>
-      <h4>Lorem Ipsum</h4>
-      <p>Lorem ipsum</p>
-      <h4>Lorem Ipsum</h4>
-      <p>Lorem ipsum</p>
-      <h4>Lorem Ipsum</h4>
-      <p>Lorem ipsum</p>
-    </article>
-    <article>
-      <h3>EDUCATION</h3>
+      <h3 className={styles.subtitle}>EDUCATION</h3>
       <hr/>
       <p4>Lorem ipsum</p4>
     </article>
