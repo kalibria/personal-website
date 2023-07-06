d6779e20b62c543e0c826afe3f85b9c2ccab198e
Maria Kazakova <mariakazakova92@gmail.com>
2023-07-06 11:45:30 +0300

    added media query

--- src/components/asideBar/AsideBar.tsx
+++ src/components/asideBar/AsideBar.tsx
@@ -16,8 +16,8 @@ export default function AsideBar () {
     <ul className={styles.contactsList}>
       <li className={styles.contactListItem}><FontAwesomeIcon icon={faPhone} className={styles.listIcon} width={14} height={14}/>+375 29 292 34 04</li>
       <li className={styles.contactListItem}><FontAwesomeIcon icon={faEnvelope} className={styles.listIcon} width={14} height={14}/>mariakazakova92@gmail.com</li>
-      <li className={styles.contactListItem}><FontAwesomeIcon icon={faGlobe} className={styles.listIcon} width={14} height={14}/>kalibria.online</li>
-      <li className={styles.contactListItem}><FontAwesomeIcon icon={faHouse} className={styles.listIcon} width={14} height={14}/>Belarus. Ready to relocate</li>
+      <li className={styles.contactListItem}><FontAwesomeIcon icon={faGlobe} className={styles.listIcon} width={14} height={14}/><a href={'https://kalibria.online'} target={'_blank'}>kalibria.online</a></li>
+      <li className={styles.contactListItem}><FontAwesomeIcon icon={faHouse} className={styles.listIcon} width={14} height={14}/>Relocating to Poland</li>
       <li className={styles.contactListItem}><Image
         src={'/github.svg'} width={14} height={14} alt={'Github'} className={styles.listIcon}/><a href={'https://github.com/kalibria'} target={'_blank'}>kalibria</a></li>
       <li className={styles.contactListItem}>
@@ -30,16 +30,11 @@ export default function AsideBar () {
       <h3 className={styles.subtitle}>SKILLS</h3>
       <hr/>
       <ul className={styles.list}>
+        <li>Javascript, Typescript</li>
+        <li>React, Redux</li>
         <li>HTML5/CSS3</li>
-        <li>Javascript</li>
-        <li>Typescript</li>
-        <li>React</li>
-        <li>Redux</li>
-        <li>Redux Toolkit</li>
         <li>Jest</li>
-        <li>React testing library</li>
-        <li>GIT</li>
-        <li>Jira</li>
+        <li>Git</li>
       </ul>
     </div>
 
@@ -58,10 +53,10 @@ export default function AsideBar () {
       <hr/>
       <ul className={styles.list} >
         <li>
-          since 2020: pet projects, documentation and theory studies, IT-centric English practice, work with mentor
+          <b>2020 - present:</b> pet projects, documentation and theory studies, IT-centric English practice, work with mentor
         </li>
         <li>
-          2009-2014: Belarusian State University of Economics
+          <b>2009 - 2014:</b> Belarusian State University of Economics
         </li>
       </ul>
     </div>
--- src/components/mainInfo/MainInfo.tsx
+++ src/components/mainInfo/MainInfo.tsx
@@ -11,30 +11,33 @@ export default function MainInfo(){
       <hr/>
       <div className={styles.textContainer}>
         <p>I am Maria, an ambitious and highly motivated developer willing to jumpstart my career in an exciting world of web technologies.</p>
-        <p>During the 2 years of my studies, I dedicated myself to learning JavaScript and React, and also battle-tested these skills by doing personal projects. I am particularly proud of Family Car Booking App, where I successfully implemented quite complex functionalities using CSS, Typescript, React and Redux. After doing multiple pet projects (you can see them all here <a href={'https://github.com/kalibria'}>github.com/kalibria</a>) over a couple of years, I am pretty sure I am ready to start developing web apps professionally.</p>
-        {/*<p><b>My skills:</b>*/}
-        {/*  <ul className={styles.list}>*/}
-        {/*    <li><b>Languages:</b> Javascript,Typescript</li>*/}
-        {/*    <li><b>React ecosystem:</b> React, Redux, Redux Toolkit, RTK Query, Formik and other React libs</li>*/}
-        {/*    <li><b>Styling:</b> CSS3, SCSS, Material UI, Bootstrap, a bit of Ant Design. Use CSS Flexbox and some CSS Grid to design web pages. Practice responsive design</li>*/}
-        {/*    <li><b>Other:</b> Know how to use Postman for backend requests, utilize Jest and React Testing Library in pet projects. Use Git CLI and know how to work with Github</li>*/}
-        {/*    </ul>*/}
-        {/*</p>*/}
+        <p>During the 2 years of my studies, I dedicated myself to learning JavaScript and React, and also battle-tested these skills by doing personal projects. I am particularly proud of <a
+          href="https://github.com/kalibria/Family-car-booking-app#readme" target={'_blank'}>Family Car Booking App</a>, where I successfully implemented quite complex functionalities using Typescript, React and Redux, CSS. </p>
+        <p className={styles.nonPrintable}>After doing multiple pet projects (you can see them all here <a href={'https://github.com/kalibria'}>github.com/kalibria</a>) over a couple of years, I am pretty sure I am ready to start developing web apps professionally.</p>
+
+          <ul className={styles.list}>
+            <b>My skills:</b>
+            <li><b>Languages:</b> Javascript, Typescript</li>
+            <li><b>React ecosystem:</b> React, Redux, Redux Toolkit, RTK Query, Formik and other React libs</li>
+            <li><b>Styling:</b> CSS3, SCSS, Material UI, Bootstrap, a bit of Ant Design. Use CSS Flexbox and some CSS Grid to design web pages. Practice responsive design</li>
+            <li><b>Other:</b> Know how to use Postman for backend requests, utilize Jest and React Testing Library in pet projects. Use Git CLI and know how to work with Github</li>
+            </ul>
+
 
           <ul className={styles.list}>
             <b>Why me:</b>
-            <li>break code into modules</li>
-            <li>understand the principles of component design</li>
-            <li>use mappers to decouple frontend data from backend specifics</li>
-            <li>always think about application structure and how my changes will affect it</li>
-            <li>work on my pet projects using Jira, decomposing tasks and creating PRs to be reviewed by the mentor</li>
-            <li>learn technologies by reading documentation as it is the best source of information</li>
+            <li>Write modular code with low coupling</li>
+            <li>Understand the principles of component design</li>
+            <li>Use mappers to decouple frontend data from backend specifics</li>
+            <li>Always think about application structure and how my changes will affect it</li>
+            <li>Work on my pet projects using Jira, decomposing tasks and creating PRs to be reviewed by the mentor</li>
+            <li>Learn technologies by reading documentation in English as it is the best source of information</li>
           </ul>
 
-        <p>For now I am working on my personal website. It will be responsive and I am planning to use TypeScript, React, Redux and Next.js.</p>
-        <p>There’s still a lot to learn of course! As for technologies, my plans include new CSS stuff like container queries and layers, as well as Storybook, Redux Saga, Styled Components, GraphQL, PWAs and more. I will continue working to increase my English language level. Long term, I am also interested in people and resource management.
+        <p className={styles.nonPrintable}>For now I am working on my personal website. It will be responsive and I am planning to use TypeScript, React, Redux and Next.js.</p>
+        <p className={styles.nonPrintable}>There’s still a lot to learn of course! As for technologies, my plans include new CSS stuff like container queries and layers, as well as Storybook, Redux Saga, Styled Components, GraphQL, PWAs and more. I will continue working to increase my English language level. Long term, I am also interested in people and resource management.
         </p>
-        <p>I am eager to work alongside experienced developers who can mentor and guide me towards becoming a proficient frontend developer.</p>
+        <p className={styles.nonPrintable}>I am eager to work alongside experienced developers who can mentor and guide me towards becoming a proficient frontend developer.</p>
         <p>In my opinion, modern web technologies, together with AI, is what are pushing many successful businesses forward, and I want to be part of this process. </p>
 
       </div>
