2220fea1286e34c27be86e167118fa9a4edab783
Maria Kazakova <mariakazakova92@gmail.com>
2023-07-28 15:47:43 +0300

    added button color when hovering

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-import styles from 'src/components/resume/resume.module.css';
+import styles from './button.module.css'
 
 interface IButton {
   inscription: string;
