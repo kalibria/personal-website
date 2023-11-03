0cbb174e2b367f9315a9f741b160c41f0344b137
Maria Kazakova <mariakazakova92@gmail.com>
2023-11-03 11:41:59 +0100

    changed color of button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import AsideBar from "@/components/asideBar/AsideBar";
-import MainInfo from "@/components/mainInfo/MainInfo";
 import Layout from "@/components/Layout";
 import Resume from '@/components/resume/resume';
 
